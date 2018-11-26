fe773dba832bebc074ba6aa5dae510cd4f090666
Erick <livingandwasting@hotmail.com>
2018-11-26 18:50:21 +0100

    front end api and model

---
+++ frontend/src/app/things/things-api.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import {API_URL} from '../env';
+import {Thing} from './thing.model';
+
+@Injectable()
+export class ThingsApiService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  private static _handleError(err: HttpErrorResponse | any) {
+    return Observable.throw(err.message || 'Error: Unable to complete request.');
+  }
+
+  // GET list of public, future events
+  getThings(): Observable<Thing[]> {
+    return this.http
+      .get(`${API_URL}/things`)
+      .catch(ThingsApiService._handleError);
+  }
+}
